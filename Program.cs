45931014a63850b2d7a2028bba0adbeaf48f2bef
95rave <ravanaap@code.edu.az>
2022-04-18 16:34:12 +0400

    task 1

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _18._04homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            number = Choose (number);
+
+            Console.Write("Please enter a number to be written first: ");
+            int newNumber = Convert.ToInt32(Console.ReadLine());
+            newNumber = Choose (newNumber);
+
+            Console.WriteLine("result" + turnNumberToNewNumber(number,newNumber));
+        }
+       
+        public static int turnNumberToNewNumber(int number, int newNumber)
+        {
+            int count = 0;
+            int Number = number;
+            while(number>0)
+            {
+                number = number / 10;
+                count++;
+            }
+            number = 1;
+            for (int i = 0; i < count; i++)
+
+            {
+                number *= 10;
+            }
+            Number += newNumber * number;
+            return Number;
+        }
+        public static int Choose(int newNumber)
+        {
+           return newNumber;
+        }
+
+    }
+}
